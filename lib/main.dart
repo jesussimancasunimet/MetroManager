e404761b3776df2e9c89e827666a450b9cf133df
jesussimancasunimet <jesussimancasunimet@gmail.com>
2025-10-25 18:32:11 -0400

    Versión inicial de MetroManager conectada a GitHub

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,14 @@
+import 'dart:ui' show PointerDeviceKind;
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+/// Paleta global
+const kDeepBlue = Color(0xFF0F3A63);
+const kAccentYellow = Color(0xFFF0B429);
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
+  runApp(const MetroManagerApp());
+}
 
